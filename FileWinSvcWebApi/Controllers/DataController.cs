59267952b1756982cfd9a1efcdee882b89b137ca
Raymond-HpZbook <remondo@gmail.com>
2020-02-02 00:55:21 +0800

    general backup

---
+++ FileWinSvcWebApi/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWinSvcWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Post()
+        {
+            Console.WriteLine("Post");
+
+            return null;
+        }
+    }
+}
